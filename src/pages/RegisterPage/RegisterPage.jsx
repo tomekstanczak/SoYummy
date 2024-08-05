b55af62cf264f3ad9889493b86d1f7e013698013
Karol Trombala <ktrombala@gmail.com>
2024-08-05 19:14:17 +0200

    add black background

---
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -0,0 +1,27 @@
+// RegisterPage.js
+
+//     Renderuje ścieżkę /register.
+//     Zawiera komponent AuthForm.
+
+import AuthNav from '../../components/Auth/AuthNav/AuthNav';
+
+import rectangle from "../../assets/images/Rectangle 8970.png";
+import styles from './RegisterPage.module.css';
+
+const RegisterPage = () => {
+  return
+  <div>
+    <img
+      src={rectangle}
+      alt="bottom black background"
+    />
+    <div>
+      
+    </div>
+    <div>
+
+    </div>
+  </div>;
+};
+
+export default RegisterPage;

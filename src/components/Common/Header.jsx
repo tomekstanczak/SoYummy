6dd02c6a6cc98abdfb4d06efa72aa3d1cafac499
tomekstanczak <tomek.stanczak87@gmail.com>
2024-08-05 10:31:10 +0200

    UserLogo Common Component

--- src/components/Common/Header.jsx
+++ src/components/Common/Header.jsx
@@ -4,9 +4,11 @@
 //   - UserLogo - przycisk otwierający okno modalne edytujące dane użytkownika;
 //   - ThemeToggler - komponent do przełączania motywu.
 
+
 // *W wersji mobilnej blok nawigacyjny i przełącznik motywu są otwierane za pomocą hamburger menu, które wyskakuje z góry i jest na całej wysokości urządzenia użytkownika.
 const Header = () => {
-  return <></>;
+  return (
+  );
 };
 
 export default Header;
--- src/components/Common/UserLogo.js
+++
@@ -1,2 +0,0 @@
-// Komponent renderuje blok ze zdjęciem użytkownika oznaczonym jako zawartość lub ikoną, jeśli nie ma takiego zdjęcia, oraz nazwą użytkownika.
-// Kliknięcie tego bloku powinno wyrenderować komponent UserLogoModal.

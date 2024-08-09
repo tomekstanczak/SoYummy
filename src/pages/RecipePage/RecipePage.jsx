1dc2077221be332fec139c3f0d0bab0d31447341
Michal901 <momot1122@wp.pl>
2024-08-09 11:07:14 +0200

    Update
    Feat: Created Context folder and RecipeContext.jsx
    Fix: Fixed BE connection

--- src/pages/RecipePage/RecipePage.jsx
+++ src/pages/RecipePage/RecipePage.jsx
@@ -1,46 +1,33 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import axios from "axios";
-
+import { RecipeContext } from "../../context/RecipeContext";
 import RecipePageHero from "../../components/RecipePage/RecipePageHero/RecipePageHero";
 import RecipeIngredientsList from "../../components/RecipePage/RecipeIngredientsList/RecipeIngredientsList";
 import RecipePreparation from "../../components/RecipePage/RecipePreparation/RecipePreparation";
-
 import styles from "./RecipePage.module.css";
 
 const RecipePage = () => {
   const { recipeId } = useParams();
-  const [recipe, setRecipe] = useState(null);
-  const [isFavorite, setIsFavorite] = useState(false);
+  const {
+    recipe,
+    isFavorite,
+    fetchRecipe,
+    setIsFavorite,
+    fetchIngredientsList,
+    ingredients: allIngredients,
+  } = useContext(RecipeContext);
 
   useEffect(() => {
-    const fetchRecipe = async () => {
-      try {
-        const response = await axios.get(
-          `https://so-yummy-31fabc853d58.herokuapp.com/recipes/recipe/${recipeId}`
-        );
-        const data = response.data.data.recipe;
-        console.log("Fetched data:", data);
-        setRecipe(data);
-        setIsFavorite(data?.favorites?.includes("yourUserId") || false);
-      } catch (error) {
-        console.error("Error fetching recipe:", error);
-      }
-    };
-
-    console.log("recipeId:", recipeId);
-    fetchRecipe();
+    fetchRecipe(recipeId);
+    fetchIngredientsList();
   }, [recipeId]);
 
   const toggleFavorite = () => {
     setIsFavorite(!isFavorite);
   };
 
-  const toggleIngredient = (ingredient) => {
-    console.log("Toggled ingredient:", ingredient);
-  };
-
   if (!recipe) return <div>Loading...</div>;
+
   return (
     <div className={styles.recipePage}>
       <RecipePageHero
@@ -50,7 +37,7 @@ const RecipePage = () => {
       />
       <RecipeIngredientsList
         ingredients={recipe.ingredients}
-        toggleIngredient={toggleIngredient}
+        allIngredients={allIngredients}
       />
       <RecipePreparation preparationSteps={recipe.instructions} />
     </div>

78bc9bfd4dfd26dab512d7d6c69326a621a9eac5
Michal901 <momot1122@wp.pl>
2024-08-10 11:53:29 +0200

    Search Page
    Feat: added pagination
    
    ERROR: Can't find which causes wrong query.

--- src/pages/SearchPage/SearchPage.jsx
+++ src/pages/SearchPage/SearchPage.jsx
@@ -1,8 +1,3 @@
-// Ułożyć stronę z wyszukiwaniem według słowa kluczowego - powinna zostać wyświetlona lista przepisów dla wybranego słowa (wersja mobilna, tabletowa i desktopowa).
-// - MainPageTitle - uniwersalny komponent tytułu strony;
-// - SerchBar - który renderuje SearchForm z MainPage i SearchTypeSelector;
-// - SearchedRecipesList - komponent renderujący listę przepisów tak samo jak na stronie CategoriesPage.
-
 import { useEffect, useState } from "react";
 import SearchBar from "../../components/SearchPage/SearchBar/SearchBar";
 import SearchedRecipesList from "../../components/SearchPage/SearchedRecipesList/SearchedRecipesList";
@@ -15,35 +10,57 @@ const SearchPage = () => {
     searchError,
     searchRecipesByTitle,
     searchRecipesByIngredient,
+    pagination,
   } = useSearch();
   const [searchValue, setSearchValue] = useState("");
   const [searchType, setSearchType] = useState("title");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [recipesPerPage, setRecipesPerPage] = useState(10);
 
   useEffect(() => {
     if (searchValue) {
       if (searchType === "title") {
-        searchRecipesByTitle(searchValue);
+        searchRecipesByTitle(searchValue, currentPage, recipesPerPage);
       } else if (searchType === "ingredient") {
-        searchRecipesByIngredient(searchValue);
+        searchRecipesByIngredient(searchValue, currentPage, recipesPerPage);
       }
     }
-  }, [
-    searchValue,
-    searchType,
-    searchRecipesByTitle,
-    searchRecipesByIngredient,
-  ]);
+  }, [searchValue, searchType, currentPage, recipesPerPage]);
 
   const handleSearch = (value, type) => {
     setSearchValue(value);
     setSearchType(type);
+    setCurrentPage(1);
   };
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+
+  const paginationButtons = [];
+  for (let i = 1; i <= Math.ceil(recipes.length / recipesPerPage); i++) {
+    paginationButtons.push(
+      <button
+        key={i}
+        onClick={() => handlePageChange(i)}
+        className={styles.paginationButton}
+      >
+        {i}
+      </button>
+    );
+  }
 
   return (
     <div className={styles.searchPage}>
       <SearchBar onSearch={handleSearch} />
       {searchError && <div className={styles.error}>{searchError}</div>}
-      <SearchedRecipesList recipes={recipes} />
+      <SearchedRecipesList
+        recipes={recipes.slice(
+          (currentPage - 1) * recipesPerPage,
+          currentPage * recipesPerPage
+        )}
+      />
+      <div className={styles.pagination}>{paginationButtons}</div>
     </div>
   );
 };

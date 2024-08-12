ff11130a9c0f4d0504ac5df273b9bdffb4623ed5
Michal901 <momot1122@wp.pl>
2024-08-12 11:41:31 +0200

    ShoppingPage & Search
    Feat: added MainPageTitle

--- src/pages/ShoppingListPage/ShoppingListPage.jsx
+++ src/pages/ShoppingListPage/ShoppingListPage.jsx
@@ -5,7 +5,7 @@
 // Komponent wykonuje żądanie kolekcji składników z listy zakupów, jeśli nie jest ona dostępna w store.
 
 import { useEffect, useContext } from "react";
-// import MainPageTitle from "../components/MainPageTitle/MainPageTitle";
+import MainPageTitle from "../../components/Common/MainPageTitle/MainPageTitle";
 import IngredientsShoppingList from "../../components/ShoppingListPage/IngredientsShoppingList/IngredientsShoppingList";
 import { RecipeContext } from "../../context/RecipeContext";
 import styles from "./ShoppingListPage.module.css";
@@ -14,7 +14,6 @@ const ShoppingListPage = () => {
   const { shoppingList, fetchShoppingList } = useContext(RecipeContext);
 
   useEffect(() => {
-    // Sprawdzamy, czy lista zakupów jest już załadowana, jeśli nie, pobieramy ją
     if (shoppingList.length === 0) {
       fetchShoppingList();
     }
@@ -22,7 +21,7 @@ const ShoppingListPage = () => {
 
   return (
     <div className={styles.container}>
-      {/* <MainPageTitle title="Shopping List" /> */}
+      <MainPageTitle title="Shopping List" />
 
       <IngredientsShoppingList ingredients={shoppingList} />
     </div>
